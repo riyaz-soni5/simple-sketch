d6624710b1c9db71acf3ac263140ea5aadc0bc26
Riyaj Soni <77921406+riyaz-soni5@users.noreply.github.com>
2023-10-05 08:58:30 +0545

    Added feature to color: color with any colors,Erase unwanted and clear to clear the canvas

--- script.js
+++ script.js
@@ -6,6 +6,64 @@ for (let i = 1; i <= 16; i++) {
     Grid.appendChild(row).classList.add('row');
     for (let j = 1; j <= 16; j++) {
         let column = document.createElement('div');
-        row.appendChild(column).classList.add('insideGrid', 'border', 'border-black','m0','col');
+        row.appendChild(column).classList.add('insideGrid', 'border', 'border-black', 'm0', 'col');
     }
 }
+
+
+
+
+let ColorGrid = document.querySelectorAll('.insideGrid');
+let userColor = document.querySelector("#color");
+let Color;
+
+userColor.oninput = (e) => Color = e.target.value;
+
+
+function colorPixels() {
+    for (let i = 0; i < ColorGrid.length; i++) {
+        ColorGrid[i].addEventListener('click', function (e) {
+            e.target.style.backgroundColor = Color;
+        })
+    }
+
+}
+
+
+// black color 
+document.querySelector('.co-black').addEventListener('click', function () {
+    Color = 'Black';
+    colorPixels();
+
+});
+
+// eraser for erasing the pixles color
+document.querySelector('.co-white').addEventListener('click', function () {
+    Color = 'White';
+    colorPixels();
+});
+
+// userColor.addEventListener('click', function () {   
+//     Color = userColor.value;
+//     Color = userColor.value;
+//     colorPixels();
+// });
+
+colorPixels();
+
+// clears the pixles and making it all white 
+document.querySelector('.clear').addEventListener('click', function () {
+    for (let i = 0; i < ColorGrid.length; i++) {
+        ColorGrid[i].style.backgroundColor = 'white';
+    }
+
+})
+
+
+
+
+
+
+
+
+
