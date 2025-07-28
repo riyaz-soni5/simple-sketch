f1af6dcd8d48884090d5085ccb525086e775df7b
Riyaj Soni <riyazsoni5@gmail.com>
2025-07-28 12:10:26 +0545

    Fix Bootstrap Issu: The bootstrap styling was not loading in the github pages not it is loading

--- script.js
+++ script.js
@@ -1,74 +1,4 @@
 
-// // generating grid 16x16
-// const Grid = document.querySelector('#grid');
-// for (let i = 1; i <= 16; i++) {
-//     let row = document.createElement('div')
-//     Grid.appendChild(row).classList.add('row');
-//     for (let j = 1; j <= 16; j++) {
-//         let column = document.createElement('div');
-//         row.appendChild(column).classList.add('insideGrid', 'border', 'border-black', 'm0', 'col', 'container-fluid');
-//     }
-// }
-
-
-
-
-// let colorGrid = document.querySelectorAll('.insideGrid');
-// let userColor = document.querySelector("#color");
-// let Color;
-
-// userColor.oninput = (e) => Color = e.target.value;
-
-
-// function colorPixels() {
-//     let mouseDownStatus = false;
-//     for (let i = 0; i < colorGrid.length; i++) {
-//         colorGrid[i].addEventListener('mousedown', function (e) {
-//             e.target.style.backgroundColor = Color;
-//             mouseDownStatus = true;
-//         });
-//         colorGrid[i].addEventListener('mouseup', function () {
-//             mouseDownStatus = false;
-//         })
-//         colorGrid[i].addEventListener('mouseover', function (e) {
-//             if (mouseDownStatus == true) {
-//                 e.target.style.backgroundColor = Color;
-//             }
-//             else {
-
-//                 return
-//             }
-//         });
-//     }
-// };
-
-
-// // black color 
-// document.querySelector('.co-black').addEventListener('click', function () {
-//     Color = 'Black';
-//     colorPixels();
-
-// });
-
-// // eraser for erasing the pixles color
-// document.querySelector('.co-white').addEventListener('click', function () {
-//     Color = 'White';
-//     colorPixels();
-// });
-
-// colorPixels();
-
-// // clears the pixles and making it all white 
-// document.querySelector('.clear').addEventListener('click', function () {
-//     for (let i = 0; i < colorGrid.length; i++) {
-//         colorGrid[i].style.backgroundColor = 'white';
-//     }
-
-// })
-
-
-
-
 const pixelContainer = document.querySelector('.pixel-container');
 const pixelChangeBtn = document.querySelector(".change-pixel");
 const clearBtn = document.querySelector('.clear');
@@ -81,6 +11,8 @@ let userPixel;
 let pixel;
 let mode;
 let colors = ["red", "orange", "yellow", "green", "blue", "indigo","violet"]
+
+alert("Pick a color and turn on your Leonardo da Vinci mode")
 
 function userCanvas(num = 16){
     pixelContainer.innerHTML = '';
